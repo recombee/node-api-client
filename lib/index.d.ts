79466ebd7578b97830c524a62e5069845f3de9b1
Ondrej Cvacho <ondrej.cvacho@recombee.com>
2025-02-08 22:40:13 +0100

    Update docs & TS definition

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -605,7 +605,7 @@ export module "recombee-api-client" {
     /**
      * Updates (some) property values of all the items that pass the filter.
      * Example: *Setting all the items that are older than a week as unavailable*
-     *   ```
+     *   ```json
      *     {
      *       "filter": "'releaseDate' < now() - 7*24*3600",
      *       "changes": {"available": false}
@@ -2002,12 +2002,113 @@ export module "recombee-api-client" {
     }
 
     /**
+     * Recommends Items that are the most relevant to a particular Segment from a context [Segmentation](https://docs.recombee.com/segmentations.html).
+     * Based on the used Segmentation, this endpoint can be used for example for:
+     * - Recommending articles related to a particular topic
+     * - Recommending songs belonging to a particular genre
+     * - Recommending products produced by a particular brand
+     * You need to set the used context Segmentation in the Admin UI in the [Scenario settings](https://docs.recombee.com/scenarios) prior to using this endpoint.
+     * The returned items are sorted by relevance (the first item being the most relevant).
+     * It is also possible to use the POST HTTP method (for example, in the case of a very long ReQL filter) — query parameters then become body parameters.
+     */
+    export class RecommendItemsToItemSegment extends requests.Request {
+      /**
+       * @param contextSegmentId - ID of the segment from `contextSegmentationId` for which the recommendations are to be generated.
+       * @param targetUserId - ID of the user who will see the recommendations.
+       * Specifying the *targetUserId* is beneficial because:
+       * * It makes the recommendations personalized
+       * * Allows the calculation of Actions and Conversions
+       *   in the graphical user interface,
+       *   as Recombee can pair the user who got recommendations
+       *   and who afterward viewed/purchased an item.
+       * If you insist on not specifying the user, pass `null`
+       * (`None`, `nil`, `NULL` etc., depending on the language) to *targetUserId*.
+       * Do not create some special dummy user for getting recommendations,
+       * as it could mislead the recommendation models,
+       * and result in wrong recommendations.
+       * For anonymous/unregistered users, it is possible to use, for example, their session ID.
+       * @param count - Number of items to be recommended (N for the top-N recommendation).
+       * @param optional - Optional parameters given as an object.
+       */
+      constructor(
+        contextSegmentId: string,
+        targetUserId: string,
+        count: number,
+        optional?: {
+          /** Scenario defines a particular application of recommendations. It can be, for example, "homepage", "cart", or "emailing". */
+          scenario?: string;
+          /** If an item of the given *itemId* or user of the given *targetUserId* doesn't exist in the database, it creates the missing entity/entities and returns some (non-personalized) recommendations. This allows, for example, rotations in the following recommendations for the user of the given *targetUserId*, as the user will be already known to the system. */
+          cascadeCreate?: boolean;
+          /** With `returnProperties=true`, property values of the recommended items are returned along with their IDs in a JSON dictionary. The acquired property values can be used to easily display the recommended items to the user.  */
+          returnProperties?: boolean;
+          /** Allows specifying which properties should be returned when `returnProperties=true` is set. The properties are given as a comma-separated list. */
+          includedProperties?: string[];
+          /** Boolean-returning [ReQL](https://docs.recombee.com/reql.html) expression, which allows you to filter recommended items based on the values of their attributes. */
+          filter?: string;
+          /** Number-returning [ReQL](https://docs.recombee.com/reql.html) expression, which allows you to boost the recommendation rate of some items based on the values of their attributes. */
+          booster?: string;
+          /** Logic specifies the particular behavior of the recommendation models. You can pick tailored logic for your domain and use case. */
+          logic?: string | object;
+          /** **Expert option** If the *targetUserId* is provided:  Specifies the threshold of how relevant must the recommended items be to the user. Possible values one of: "low", "medium", "high". The default value is "low", meaning that the system attempts to recommend a number of items equal to *count* at any cost. If there is not enough data (such as interactions or item properties), this may even lead to bestseller-based recommendations being appended to reach the full *count*. This behavior may be suppressed by using "medium" or "high" values. In such case, the system only recommends items of at least the requested relevance and may return less than *count* items when there is not enough data to fulfill it. */
+          minRelevance?: string;
+          /** **Expert option** If the *targetUserId* is provided: If your users browse the system in real-time, it may easily happen that you wish to offer them recommendations multiple times. Here comes the question: how much should the recommendations change? Should they remain the same, or should they rotate? Recombee API allows you to control this per request in a backward fashion. You may penalize an item for being recommended in the near past. For the specific user, `rotationRate=1` means maximal rotation, `rotationRate=0` means absolutely no rotation. You may also use, for example, `rotationRate=0.2` for only slight rotation of recommended items. */
+          rotationRate?: number;
+          /** **Expert option** If the *targetUserId* is provided: Taking *rotationRate* into account, specifies how long it takes for an item to recover from the penalization. For example, `rotationTime=7200.0` means that items recommended less than 2 hours ago are penalized. */
+          rotationTime?: number;
+          /** Dictionary of custom options. */
+          expertSettings?: { [key: string]: unknown };
+          /** If there is a custom AB-testing running, return the name of the group to which the request belongs. */
+          returnAbGroup?: boolean;
+        }
+      );
+
+      contextSegmentId: string;
+      targetUserId: string;
+      count: number;
+      scenario?: string;
+      cascadeCreate?: boolean;
+      returnProperties?: boolean;
+      includedProperties?: string[];
+      filter?: string;
+      booster?: string;
+      logic?: string | object;
+      minRelevance?: string;
+      rotationRate?: number;
+      rotationTime?: number;
+      expertSettings?: { [key: string]: unknown };
+      returnAbGroup?: boolean;
+      protected __response_type: RecommendationResponse;
+
+      bodyParameters(): {
+        contextSegmentId: string;
+        targetUserId: string;
+        count: number;
+        scenario?: string;
+        cascadeCreate?: boolean;
+        returnProperties?: boolean;
+        includedProperties?: string[];
+        filter?: string;
+        booster?: string;
+        logic?: string | object;
+        minRelevance?: string;
+        rotationRate?: number;
+        rotationTime?: number;
+        expertSettings?: { [key: string]: unknown };
+        returnAbGroup?: boolean;
+      };
+
+      queryParameters(): {
+      };
+    }
+
+    /**
      * Returns items that shall be shown to a user as next recommendations when the user e.g. scrolls the page down (*infinite scroll*) or goes to the next page.
      * It accepts `recommId` of a base recommendation request (e.g., request from the first page) and the number of items that shall be returned (`count`).
      * The base request can be one of:
-     *   - [Recommend items to item](https://docs.recombee.com/api.html#recommend-items-to-item)
-     *   - [Recommend items to user](https://docs.recombee.com/api.html#recommend-items-to-user)
-     *   - [Search items](https://docs.recombee.com/api.html#search-items)
+     *   - [Recommend Items to Item](https://docs.recombee.com/api.html#recommend-items-to-item)
+     *   - [Recommend Items to User](https://docs.recombee.com/api.html#recommend-items-to-user)
+     *   - [Recommend Items to Item Segment](https://docs.recombee.com/api.html#recommend-items-to-item-segment)
+     *   - [Search Items](https://docs.recombee.com/api.html#search-items)
      * All the other parameters are inherited from the base request.
      * *Recommend next items* can be called many times for a single `recommId` and each call returns different (previously not recommended) items.
      * The number of *Recommend next items* calls performed so far is returned in the `numberNextRecommsCalls` field.
@@ -2404,106 +2505,6 @@ export module "recombee-api-client" {
         filter?: string;
         booster?: string;
         logic?: string | object;
-        expertSettings?: { [key: string]: unknown };
-        returnAbGroup?: boolean;
-      };
-
-      queryParameters(): {
-      };
-    }
-
-    /**
-     * Recommends Items that are the most relevant to a particular Segment from a context [Segmentation](https://docs.recombee.com/segmentations.html).
-     * Based on the used Segmentation, this endpoint can be used for example for:
-     * - Recommending articles related to a particular topic
-     * - Recommending songs belonging to a particular genre
-     * - Recommending products produced by a particular brand
-     * You need to set the used context Segmentation in the Admin UI in the [Scenario settings](https://docs.recombee.com/scenarios) prior to using this endpoint.
-     * The returned items are sorted by relevance (the first item being the most relevant).
-     * It is also possible to use the POST HTTP method (for example, in the case of a very long ReQL filter) — query parameters then become body parameters.
-     */
-    export class RecommendItemsToItemSegment extends requests.Request {
-      /**
-       * @param contextSegmentId - ID of the segment from `contextSegmentationId` for which the recommendations are to be generated.
-       * @param targetUserId - ID of the user who will see the recommendations.
-       * Specifying the *targetUserId* is beneficial because:
-       * * It makes the recommendations personalized
-       * * Allows the calculation of Actions and Conversions
-       *   in the graphical user interface,
-       *   as Recombee can pair the user who got recommendations
-       *   and who afterward viewed/purchased an item.
-       * If you insist on not specifying the user, pass `null`
-       * (`None`, `nil`, `NULL` etc., depending on the language) to *targetUserId*.
-       * Do not create some special dummy user for getting recommendations,
-       * as it could mislead the recommendation models,
-       * and result in wrong recommendations.
-       * For anonymous/unregistered users, it is possible to use, for example, their session ID.
-       * @param count - Number of items to be recommended (N for the top-N recommendation).
-       * @param optional - Optional parameters given as an object.
-       */
-      constructor(
-        contextSegmentId: string,
-        targetUserId: string,
-        count: number,
-        optional?: {
-          /** Scenario defines a particular application of recommendations. It can be, for example, "homepage", "cart", or "emailing". */
-          scenario?: string;
-          /** If an item of the given *itemId* or user of the given *targetUserId* doesn't exist in the database, it creates the missing entity/entities and returns some (non-personalized) recommendations. This allows, for example, rotations in the following recommendations for the user of the given *targetUserId*, as the user will be already known to the system. */
-          cascadeCreate?: boolean;
-          /** With `returnProperties=true`, property values of the recommended items are returned along with their IDs in a JSON dictionary. The acquired property values can be used to easily display the recommended items to the user.  */
-          returnProperties?: boolean;
-          /** Allows specifying which properties should be returned when `returnProperties=true` is set. The properties are given as a comma-separated list. */
-          includedProperties?: string[];
-          /** Boolean-returning [ReQL](https://docs.recombee.com/reql.html) expression, which allows you to filter recommended items based on the values of their attributes. */
-          filter?: string;
-          /** Number-returning [ReQL](https://docs.recombee.com/reql.html) expression, which allows you to boost the recommendation rate of some items based on the values of their attributes. */
-          booster?: string;
-          /** Logic specifies the particular behavior of the recommendation models. You can pick tailored logic for your domain and use case. */
-          logic?: string | object;
-          /** **Expert option** If the *targetUserId* is provided:  Specifies the threshold of how relevant must the recommended items be to the user. Possible values one of: "low", "medium", "high". The default value is "low", meaning that the system attempts to recommend a number of items equal to *count* at any cost. If there is not enough data (such as interactions or item properties), this may even lead to bestseller-based recommendations being appended to reach the full *count*. This behavior may be suppressed by using "medium" or "high" values. In such case, the system only recommends items of at least the requested relevance and may return less than *count* items when there is not enough data to fulfill it. */
-          minRelevance?: string;
-          /** **Expert option** If the *targetUserId* is provided: If your users browse the system in real-time, it may easily happen that you wish to offer them recommendations multiple times. Here comes the question: how much should the recommendations change? Should they remain the same, or should they rotate? Recombee API allows you to control this per request in a backward fashion. You may penalize an item for being recommended in the near past. For the specific user, `rotationRate=1` means maximal rotation, `rotationRate=0` means absolutely no rotation. You may also use, for example, `rotationRate=0.2` for only slight rotation of recommended items. */
-          rotationRate?: number;
-          /** **Expert option** If the *targetUserId* is provided: Taking *rotationRate* into account, specifies how long it takes for an item to recover from the penalization. For example, `rotationTime=7200.0` means that items recommended less than 2 hours ago are penalized. */
-          rotationTime?: number;
-          /** Dictionary of custom options. */
-          expertSettings?: { [key: string]: unknown };
-          /** If there is a custom AB-testing running, return the name of the group to which the request belongs. */
-          returnAbGroup?: boolean;
-        }
-      );
-
-      contextSegmentId: string;
-      targetUserId: string;
-      count: number;
-      scenario?: string;
-      cascadeCreate?: boolean;
-      returnProperties?: boolean;
-      includedProperties?: string[];
-      filter?: string;
-      booster?: string;
-      logic?: string | object;
-      minRelevance?: string;
-      rotationRate?: number;
-      rotationTime?: number;
-      expertSettings?: { [key: string]: unknown };
-      returnAbGroup?: boolean;
-      protected __response_type: RecommendationResponse;
-
-      bodyParameters(): {
-        contextSegmentId: string;
-        targetUserId: string;
-        count: number;
-        scenario?: string;
-        cascadeCreate?: boolean;
-        returnProperties?: boolean;
-        includedProperties?: string[];
-        filter?: string;
-        booster?: string;
-        logic?: string | object;
-        minRelevance?: string;
-        rotationRate?: number;
-        rotationTime?: number;
         expertSettings?: { [key: string]: unknown };
         returnAbGroup?: boolean;
       };
